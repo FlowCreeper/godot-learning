077600ae88ec044a9c817e906106ad9666825184
DragonBW <lucasmffreitas@hotmail.com>
2024-08-22 17:28:43 -0300

    V0.11

--- Mov.gd
+++ Mov.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 @export var Velocity = 0.0
-const gravity = 10.0
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = Vector2.ZERO
-@export var jump = 0.0     
+@export var jump = 0.0
+	 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,12 +17,16 @@ func _process(delta: float) -> void:
 		direction.x += 1
 	if Input.is_action_pressed("MoveL"):	
 		direction.x -= 1
-	if Input.is_action_just_pressed("Jump"):
-		direction.y = 1
-	position.x += direction.x * delta * Velocity * 100
 	
+	position.x += direction.x * delta * Velocity * 100
+
 func _physics_process(delta: float) -> void:
-	move_and_collide(Vector2(0,1)*gravity*delta)
-	if direction.y == 1:
-		position.y += -direction.y * jump * delta * 100
+	if not is_on_floor():
+		velocity.y += gravity*delta
+	
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = -jump
+		print(velocity.y)
+	
+	move_and_slide()
 	
